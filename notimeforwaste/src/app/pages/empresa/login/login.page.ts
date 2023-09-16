8f90c3f0146ae9d612f6385219b657b0f80dc32c
ArthurQuintanilha <arthur@kxptech.com>
2023-09-16 10:36:15 -0300

    initial commit

--- notimeforwaste/src/app/pages/empresa/login/login.page.ts
+++ notimeforwaste/src/app/pages/empresa/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ToastController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  email?: String;
+  senha?: String;
+  formGroup: FormGroup;
 
-  constructor() { }
+  constructor(private fBuilder: FormBuilder, private activatedRoute: ActivatedRoute, private toastController: ToastController, private navController: NavController) {
 
+    this.formGroup = this.fBuilder.group(
+      {
+        'identificacao': [this.email, Validators.compose([
+          Validators.required
+        ])],
+        'observacao': [this.senha, Validators.compose([
+          Validators.required
+        ])],
+
+      }
+    );
+  }
+
+  async salvar(){
+    
+  }
   ngOnInit() {
   }
 
