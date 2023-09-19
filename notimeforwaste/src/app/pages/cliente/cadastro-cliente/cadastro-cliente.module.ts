abbe51c3b53d2d71e4e443732ffd799b9f44d232
Dev-ramalho <138873875+BarechalFonseca@users.noreply.github.com>
2023-09-19 18:24:55 -0300

    Feito o Cadastro

--- notimeforwaste/src/app/pages/cliente/cadastro-cliente/cadastro-cliente.module.ts
+++ notimeforwaste/src/app/pages/cliente/cadastro-cliente/cadastro-cliente.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { CadastroClientePageRoutingModule } from './cadastro-cliente-routing.module';
 
 import { CadastroClientePage } from './cadastro-cliente.page';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CadastroClientePageRoutingModule
+    CadastroClientePageRoutingModule,
+    FontAwesomeModule,
+    ReactiveFormsModule
   ],
   declarations: [CadastroClientePage]
 })
