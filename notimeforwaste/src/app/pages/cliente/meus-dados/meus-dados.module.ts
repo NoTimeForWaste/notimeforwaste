f685e3271d795649bb578ac00b6de3168e738187
Dev-ramalho <138873875+BarechalFonseca@users.noreply.github.com>
2023-09-26 18:33:22 -0300

    Fiz

--- notimeforwaste/src/app/pages/cliente/meus-dados/meus-dados.module.ts
+++ notimeforwaste/src/app/pages/cliente/meus-dados/meus-dados.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { MeusDadosPageRoutingModule } from './meus-dados-routing.module';
 
 import { MeusDadosPage } from './meus-dados.page';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    MeusDadosPageRoutingModule
+    MeusDadosPageRoutingModule,
+    FontAwesomeModule,
+    ReactiveFormsModule
   ],
   declarations: [MeusDadosPage]
 })
