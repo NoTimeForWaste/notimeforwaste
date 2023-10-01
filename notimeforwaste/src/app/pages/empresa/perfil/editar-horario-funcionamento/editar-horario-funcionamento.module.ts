08e5e95850178b6451d5032ea850bc0de1bbb11d
Dev-ramalho <138873875+BarechalFonseca@users.noreply.github.com>
2023-10-01 19:01:26 -0300

    Feito

--- notimeforwaste/src/app/pages/empresa/perfil/editar-horario-funcionamento/editar-horario-funcionamento.module.ts
+++ notimeforwaste/src/app/pages/empresa/perfil/editar-horario-funcionamento/editar-horario-funcionamento.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { EditarHorarioFuncionamentoPageRoutingModule } from './editar-horario-funcionamento-routing.module';
 
 import { EditarHorarioFuncionamentoPage } from './editar-horario-funcionamento.page';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    EditarHorarioFuncionamentoPageRoutingModule
+    EditarHorarioFuncionamentoPageRoutingModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    NgxMaskDirective, 
+    NgxMaskPipe
   ],
   declarations: [EditarHorarioFuncionamentoPage]
 })
--- notimeforwaste/src/app/pages/empresa/perfil/editar-horario-funcionamento/editar-horario-funcionamento.page.ts
+++ notimeforwaste/src/app/pages/empresa/perfil/editar-horario-funcionamento/editar-horario-funcionamento.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HorarioFuncionamento } from 'src/app/model/horario-funcionamento';
 
 @Component({
   selector: 'app-editar-horario-funcionamento',
@@ -7,7 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarHorarioFuncionamentoPage implements OnInit {
 
-  constructor() { }
+  formGroup: FormGroup;
+  horarioSegASex: HorarioFuncionamento;
+  horarioSabado: HorarioFuncionamento;
+  horarioDomingo: HorarioFuncionamento;
+
+  constructor(private fBuilder: FormBuilder) {
+    this.horarioDomingo = new HorarioFuncionamento();
+    this.horarioSabado = new HorarioFuncionamento();
+    this.horarioSegASex = new HorarioFuncionamento();
+
+    this.formGroup = this.fBuilder.group({
+      'hrSegASexI': [this.horarioSegASex.horarioInicial, Validators.compose([
+        Validators.required
+      ])],
+      'hrSegASexF': [this.horarioSegASex.horarioFinal, Validators.compose([
+        Validators.required
+      ])],
+      'hrSabadoI': [this.horarioSabado.horarioInicial, Validators.compose([
+        Validators.required
+      ])],
+      'hrSabadoF': [this.horarioSabado.horarioFinal, Validators.compose([
+        Validators.required
+      ])],
+      'hrDomingoI': [this.horarioDomingo.horarioInicial, Validators.compose([
+        Validators.required
+      ])],
+      'hrDomingoF': [this.horarioDomingo.horarioFinal, Validators.compose([
+        Validators.required
+      ])]
+    }
+    )
+
+    
+   }
 
   ngOnInit() {
   }
